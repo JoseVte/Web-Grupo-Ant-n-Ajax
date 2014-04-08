d1f894d5f858368e1aaeae5513d7cbd802cd668d
Jose Vte Orts Romero <jvortsromero@gmail.com>
2014-04-08 16:17:40 +0200

    Primer Sprint

--- Proyecto/Paginas/Contabilidad.php
+++ Proyecto/Paginas/Contabilidad.php
@@ -1,3 +1,27 @@
-Contabilidad
-
-<?php
+    <form method="POST" action="">
+        <table>
+            <tr>
+                <td><label for="numero">Nº</label></td>
+                <td><input type="text" name="numero" id="txtNumero" required="true"></td>
+            </tr>
+            <tr>
+                <td><label for="nifcif">NIF/CIF</label></td>
+                <td><input type="text" name="nifcif" id="txtNifCif" required="true"></td>
+            </tr>
+            <tr>
+                <td><label for="telefono">Teléfono</label></td>
+                <td><input type="text" name="telefono" id="txtTelefono" required="true"></td>
+            </tr>
+            <tr>
+                <td><label for="poblacion">Población</label></td>
+                <td><input type="text" name="poblacion" id="txtPoblacion" required="true"></td>
+            </tr>
+            <tr>
+                <td><label for="direccion">Dirección</label></td>
+                <td><input type="text" name="direccion" id="txtDireccion" required="true"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" id="generar" value="Generar"></td>
+            </tr>
+        </table>
+    </form>
